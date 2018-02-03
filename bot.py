c894de8e0959190503d54f2851b43a59ebb107f0
Arvid Jakobsson <arvid.jakobsson@gmail.com>
2018-02-03 12:14:30 +0100

    disable thanking

--- bot.py
+++ bot.py
@@ -17,13 +17,13 @@ bot = Bot(
 )
 logger = logging.getLogger("levernebot")
 
-thxm8Regexp = r'(^| )([Tt][Aa][Cc][Kk])( |$|[\.\?!])'
-@bot.command(thxm8Regexp)
-def do_correct_thx(chat, match):
-    logger.info("one person (%s) thanked", chat.sender)
-    used = match.group(2)
-    correction = "Tacka ja till allt, tacka inte för något."
-    return chat.send_text(correction)
+# thxm8Regexp = r'(^| )([Tt][Aa][Cc][Kk])( |$|[\.\?!])'
+# @bot.command(thxm8Regexp)
+# def do_correct_thx(chat, match):
+#     logger.info("one person (%s) thanked", chat.sender)
+#     used = match.group(2)
+#     correction = "Tacka ja till allt, tacka inte för något."
+#     return chat.send_text(correction)
 
 # # if someone uses de/dem, correct them.
 # deDemRegexp = r'(^| )([Dd]e(m?))( |$|[\.\?!])'
